1ad43754606a9e4f1a14f36eec450a18ef13766d
alongithub <1792108796@qq.com>
2020-08-14 16:02:25 +0800

    absolute实现设置宽高，拖拽放置，选中删除

--- src/index.js
+++ src/index.js
@@ -3,15 +3,25 @@ import './style.less'
 const {log, warn, dir} = console;
 
 const n = 24;
-const m = 10;
+const m = 21;
 
 const ln = 4;
-const lm = 10
+const lm = 4;
+
+
+let index = 1;
 
 const item = document.getElementById('item')
 const wrapper = document.getElementById('wrapper') 
-const view = document.getElementById('view')
+const view = document.getElementById('view_wrapper')
 
+let target = null;
+const delete_btn = document.getElementById('delete_btn')
+delete_btn.onclick = () => {
+    if (target) 
+    view.removeChild(target);
+    target = null;
+}
 
 function getStyle(attr) {
     if (this.currentStyle) {
@@ -21,15 +31,34 @@ function getStyle(attr) {
     }
 }
 
+
+
+// view.onclick = (e) => {
+//     const el = e.target;
+//     if(el.className === 'hightlight') {
+//         console.log(el)
+//         target = el;
+//     }
+// }
+
+view.onmousedown = (e) => {
+    const el = e.target;
+    if(el.className === 'hightlight') {
+        target = el;
+
+    }
+}
+
 item.onmousedown = (e) => {
+    const ln = parseInt(document.getElementById('ln').value) || 4;
+    const lm = parseInt(document.getElementById('lm').value) || 4;
     let x = e.clientX;
     let y = e.clientY;
     let hascreate = false;
     let left = parseInt(getStyle.bind(item)( 'left'), 10)
     let top = parseInt(getStyle.bind(item)('top'), 10);
-
-    const leftlowitem = document.createElement('div')
-    const rightlowitem = document.createElement('div')
+    const highlight = document.createElement('div')
+    
     wrapper.onmousemove = (e) => {
         const moveX = e.clientX - x;
         const moveY = e.clientY - y;
@@ -41,49 +70,57 @@ item.onmousedown = (e) => {
         const cy = e.clientY
         // 判断鼠标是否在画布内部
 
-        const viewleft = view.offsetLeft
+        // view.offsetLeft 和 clientLeft 获取的是相对父元素的位置
+        const viewleft = view.getBoundingClientRect().left
         const viewwidth = view.offsetWidth
-        const viewtop = view.offsetTop
+        const viewtop = view.getBoundingClientRect().top
         const viewheight = view.offsetHeight
 
-        const sw = viewwidth / n
-        const sh = viewheight / m
+        const sw = viewwidth / n // 单个格子宽度
+        const sh = viewheight / m // 单个格子高度
         if (
-            cx > view.offsetLeft
-            && cy > view.offsetTop 
+            cx > viewleft
+            && cy > viewtop
             && cx < (viewleft + viewwidth)
             && cy < (viewtop + viewheight)
         ) {
             // 计算 匹配位置
             // 计算鼠标相对画布的位置
-            const vl = cx - viewleft;
-            const vt = cy - viewtop;
+            const vl = cx - viewleft; // 鼠标相对画布远点的x偏移量
+            const vt = cy - viewtop; // 鼠标相对于画布原点的y偏移量
             // 计算最佳匹配的格子
-            const s = parseInt(vl / sw)
+            const tx = parseInt(vl / sw) 
+            const ty = parseInt(vt / sh)
 
-            const slt = s + ln > n ? n - ln : s;
-            const srt = s + ln > n ? n : s + ln;
-            console.log(slt + '~' + srt)
+            const sx = tx + ln > n ? n - ln : tx;
+            const sy = ty + lm > m ? m - lm : ty;
+            // console.log(sx + '~' + sy)
             // 创建一个高亮
             if (!hascreate) {
                 hascreate = true; // 避免重复创建
-                const highlight = document.createElement('div')
+                target = highlight
                 highlight.className = 'hightlight';
-                highlight.style.flex = ln;
-
-                
-                leftlowitem.className = 'lowlight';
-                leftlowitem.style.flex = slt;
-
-                
-                rightlowitem.className = 'lowlight';
-                rightlowitem.style.flex = n - ln - slt;
-                view.append(leftlowitem)
+                // warn(sx)
+                highlight.style.width = ln * sw + 'px';
+                highlight.style.height = lm * sh + 'px';
+                highlight.style.left = sx * sw + 'px';
+                highlight.style.top = sy * sh + 'px';
+                highlight.style.zIndex = index;
+                highlight.innerHTML = index
+                highlight.style.background = `rgb(${Math.random()*255},${Math.random()*255}, ${Math.random()*255})`
+                index ++;
+                highlight.dataset.ln = ln;
+                highlight.dataset.lm = lm;
+                highlight.dataset.sx = sx;
+                highlight.dataset.sy = sy;
                 view.append(highlight)
-                view.append(rightlowitem)
+
+
             } else {
-                leftlowitem.style.flex = slt;
-                rightlowitem.style.flex = n - ln - slt;
+                highlight.style.left = sx * sw + 'px';
+                highlight.style.top = sy * sh + 'px';
+                highlight.dataset.sx = sx;
+                highlight.dataset.sy = sy;
             }
             
         }
